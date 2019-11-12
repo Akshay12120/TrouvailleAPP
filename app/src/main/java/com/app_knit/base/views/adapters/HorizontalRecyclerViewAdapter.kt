2f081d00c82f88004d70084a9f24eb19b6bddad5
akshay <akshay.appknit@gmail.com>
2019-11-12 18:34:32 +0530

    update: added horizontal recyclerview in All places fragment

---
+++ app/src/main/java/com/app_knit/base/views/adapters/HorizontalRecyclerViewAdapter.kt
@@ -0,0 +1,46 @@
+package com.app_knit.base.views.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import androidx.recyclerview.widget.RecyclerView
+import com.app_knit.base.R
+import com.app_knit.base.repository.models.BlogPostLocations
+import kotlinx.android.synthetic.main.row_loactions.view.*
+
+class HorizontalRecyclerViewAdapter :RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    private var items: List<BlogPostLocations> = ArrayList()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return BlogviewHolder(
+                LayoutInflater.from(parent.context).inflate(R.layout.row_loactions,parent,false)
+
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is BlogviewHolder->{
+               holder.bind(items.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+    fun submitList(blogList: List<BlogPostLocations>){
+        items = blogList
+    }
+    class BlogviewHolder constructor(itemView: View):RecyclerView.ViewHolder(itemView){
+        val blog_location = itemView.txTv_location_paris
+        val blog_location2 = itemView.txTvmuseum
+        fun bind(blogPostLocations: BlogPostLocations){
+            blog_location.setText(blogPostLocations.loction1)
+            blog_location2.setText(blogPostLocations.location2)
+
+        }
+    }
+
+
+}
--- app/src/main/java/com/app_knit/base/views/fragments/AllPlacesFragment.kt
+++ app/src/main/java/com/app_knit/base/views/fragments/AllPlacesFragment.kt
@@ -6,17 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 import com.app_knit.base.R
 import com.app_knit.base.repository.models.DataSourceAllplaces
+import com.app_knit.base.repository.models.DataSourceLocations
+import com.app_knit.base.repository.models.DataSourceNotification
 import com.app_knit.base.viewmodels.BaseViewModel
 import com.app_knit.base.views.adapters.AllplacesAdapter
+import com.app_knit.base.views.adapters.HorizontalRecyclerViewAdapter
+import kotlinx.android.synthetic.main.fragment_all_places.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class AllPlacesFragment : BaseRecyclerViewFragment() {
+
+    private  lateinit var blogAdapter: HorizontalRecyclerViewAdapter
+
     override val layoutId: Int
         get() = R.layout.fragment_all_places
     override val viewModel: BaseViewModel?
@@ -25,7 +34,26 @@ class AllPlacesFragment : BaseRecyclerViewFragment() {
     override fun setData() {
         val data = DataSourceAllplaces.createdata()
         allplacesAdapter.submitList(data)
+        initRecyclerView()
+        addDataSet()
     }
+    private fun addDataSet(){
+        val data = DataSourceLocations.createdata()
+        blogAdapter.submitList(data)
+    }
+    private fun initRecyclerView(){
+        recyclerView_horizontal.apply {
+            layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+           // LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            blogAdapter = HorizontalRecyclerViewAdapter()
+            adapter = blogAdapter
+        }
+        recyclerView_horizontal.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+        //blogAdapter = HorizontalRecyclerViewAdapter()
+        recyclerView_horizontal.adapter = blogAdapter
+    }
+
+
         private val allplacesAdapter = AllplacesAdapter()
     override val recyclerViewAdapter: RecyclerView.Adapter<*>?
         get() = allplacesAdapter
