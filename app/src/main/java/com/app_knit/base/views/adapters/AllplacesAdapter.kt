d01a8d42b2ed7299176e8ea3bb30578f0264129d
akshay <akshay.appknit@gmail.com>
2019-11-12 13:31:42 +0530

    update: All places fragment and visited fragment completed with vertical recycler view

---
+++ app/src/main/java/com/app_knit/base/views/adapters/AllplacesAdapter.kt
@@ -0,0 +1,51 @@
+package com.app_knit.base.views.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.app_knit.base.R
+import com.app_knit.base.repository.models.BlogPostAllPlaces
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.row_all_placeslist.view.*
+
+
+class AllplacesAdapter :RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    private var items: List<BlogPostAllPlaces> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return Blogviewholder(
+                LayoutInflater.from(parent.context).inflate(R.layout.row_all_placeslist, parent, false)
+        )
+    }
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is Blogviewholder ->{
+                holder.bind(items.get(position))
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+       return items.size
+    }
+    fun submitList(blogList: List<BlogPostAllPlaces>) {
+        items = blogList
+        notifyDataSetChanged()
+    }
+    class Blogviewholder constructor(itemView: View):RecyclerView.ViewHolder(itemView){
+        val blog_imag = itemView.iv_image_places
+        val blog_name = itemView.txTv_triomphe
+        val blog_location = itemView.txTvLocation
+        val blog_friendsvisited = itemView.txTv_two_friends
+        fun bind(blogPostAllPlaces: BlogPostAllPlaces){
+            blog_name.setText(blogPostAllPlaces.name)
+            blog_location.setText(blogPostAllPlaces.loaction)
+            blog_friendsvisited.setText(blogPostAllPlaces.friends)
+            Glide.with(itemView.context)
+                    .load(blogPostAllPlaces.imgplace)
+                    .into(blog_imag)
+        }
+    }
+
+}
--- app/src/main/java/com/app_knit/base/views/fragments/VisitedbyFriendsFragment.kt
+++ app/src/main/java/com/app_knit/base/views/fragments/VisitedbyFriendsFragment.kt
@@ -6,19 +6,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
 import com.app_knit.base.R
+import com.app_knit.base.repository.models.DataSourceAllplaces
+import com.app_knit.base.viewmodels.BaseViewModel
+import com.app_knit.base.views.adapters.AllplacesAdapter
 
 /**
  * A simple [Fragment] subclass.
  */
-class VisitedbyFriendsFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_visitedby_friends, container, false)
+class VisitedbyFriendsFragment : BaseRecyclerViewFragment() {
+    override fun setData() {
+        val data = DataSourceAllplaces.createdata()
+        allplacesAdapter.submitList(data)
     }
+    private val allplacesAdapter = AllplacesAdapter()
+    override val recyclerViewAdapter: RecyclerView.Adapter<*>?
+        get() = allplacesAdapter
+    override val layoutManager: RecyclerView.LayoutManager?
+        get() = null
+    override val isShowRecyclerViewDivider: Boolean
+        get() = false
+    override fun onPullDownToRefresh() {
+
+    }
+
+    override val layoutId: Int
+        get() = R.layout.fragment_all_places
+    override val viewModel: BaseViewModel?
+        get() = null
 
 
 }
