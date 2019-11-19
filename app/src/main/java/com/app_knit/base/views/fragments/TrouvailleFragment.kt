62897f84aa08f75ea1794b4d5fabac2f0c472f40
akshay <akshay.appknit@gmail.com>
2019-11-19 13:01:51 +0530

    update:TrouvailleFragment done

--- app/src/main/java/com/app_knit/base/views/fragments/TrouvailleFragment.kt
+++ app/src/main/java/com/app_knit/base/views/fragments/TrouvailleFragment.kt
@@ -1,23 +1,59 @@
 package com.app_knit.base.views.fragments
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.app_knit.base.R
+import com.app_knit.base.viewmodels.BaseViewModel
+import com.app_knit.base.views.activities.BaseAppCompactActivity
+import com.app_knit.base.views.activities.doFragmentTransaction
+import kotlinx.android.synthetic.main.fragment_trouvaille.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class TrouvailleFragment : Fragment() {
+class TrouvailleFragment : BaseFragment(),View.OnClickListener {
+    override val layoutId: Int
+        get() = R.layout.fragment_trouvaille
+    override val viewModel: BaseViewModel?
+        get() = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        return TextView(activity).apply {
-            setText(R.string.hello_blank_fragment)
+    override fun init() {
+//        signinBtn.setOnClickListener { (activity as BaseAppCompactActivity).doFragmentTransaction(fragManager = activity!!.supportFragmentManager, containerViewId = R.id.flFragContainerMain, fragment = WelcomeTrouvailleFragment(), isAddFragment = false) }
+//        signin_GoogleBtn.setOnClickListener { () }
+        signinBtn.setOnClickListener(this)
+        signin_GoogleBtn.setOnClickListener(this)
+        signin_FacebookBtn.setOnClickListener(this)
+        txtv_create_account.setOnClickListener(this)
+        //activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        //activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        //activity!!.window.statusBarColor = ContextCompat.getColor(activity!!, R.color.colorTransparent)
+        //activity!!.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            R.id.signinBtn ->{
+                (activity as BaseAppCompactActivity).doFragmentTransaction(fragManager = activity!!.supportFragmentManager, containerViewId = R.id.flFragContainerMain, fragment = WelcomeTrouvailleFragment(), isAddFragment = false)
+            }
+            R.id.signin_GoogleBtn ->{
+                (activity as BaseAppCompactActivity).doFragmentTransaction(fragManager = activity!!.supportFragmentManager, containerViewId =R.id.flFragContainerMain, fragment = BottomNavigationTabLayoutFragment(), isAddFragment = false  )
+            }
+            R.id.signin_FacebookBtn ->{
+                (activity as BaseAppCompactActivity).doFragmentTransaction(fragManager = activity!!.supportFragmentManager, containerViewId =R.id.flFragContainerMain, fragment = BottomNavigationTabLayoutFragment(), isAddFragment = false  )
+            }
+            R.id.txtv_create_account ->{
+                (activity as BaseAppCompactActivity).doFragmentTransaction(fragManager = activity!!.supportFragmentManager, containerViewId =R.id.flFragContainerMain, fragment = SetupAccountFragment(), isAddFragment = false  )
+            }
+
         }
     }
 
